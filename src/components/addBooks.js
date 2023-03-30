b0dfea75e263e22819d05bb2f46cd90e740ca205
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-03-30 18:46:17 +0300

    Create addBooks.js

---
+++ src/components/addBooks.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AddBook = () => (
+  <div>
+    <form>
+      <input type="text" placeholder="Title" />
+      <input type="text" placeholder="Author" />
+      <input type="submit" value="Add Book" />
+    </form>
+  </div>
+);
+
+export default AddBook;
