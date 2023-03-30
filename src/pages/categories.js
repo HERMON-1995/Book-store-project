5b0e8a2fdce64aa42afd033be7886799aca82592
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-03-30 18:46:58 +0300

    Create categories.js

---
+++ src/pages/categories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Categories = () => (
+  <div className="status-div">
+    <button type="submit" className="status-btn">Check Status</button>
+  </div>
+);
+export default Categories;
