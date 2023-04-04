2f5be3c56dab5ae4aed5b180a109d893a47a7afc
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-04-04 13:14:37 +0300

    Update navBar.js

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => (
   <nav>
-    <h1 className="book-h1">Bookstore CMS</h1>
     <Link to="/">Books</Link>
     <Link to="/categories">Categories</Link>
   </nav>
