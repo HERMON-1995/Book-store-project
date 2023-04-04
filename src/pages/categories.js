8005141e430019446dba54c627c0288ab2f0754d
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-04-04 13:14:44 +0300

    Update categories.js

--- src/pages/categories.js
+++ src/pages/categories.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categoriesSlice';
 
-const Categories = () => (
-  <div className="status-div">
-    <button type="submit" className="status-btn">Check Status</button>
-  </div>
-);
+const Categories = () => {
+  const allCategories = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+
+  const dispatchAction = () => {
+    dispatch(checkStatus());
+  };
+
+  return (
+    <div className="status-div">
+      <button type="submit" className="status-btn" onClick={dispatchAction()}>Check Status</button>
+      <h3>{allCategories}</h3>
+    </div>
+  );
+};
 export default Categories;
