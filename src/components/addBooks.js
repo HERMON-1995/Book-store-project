da061a59d27b264a18ad0a6e69d68f255af02e24
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-04-04 13:14:27 +0300

    Update addBooks.js

--- src/components/addBooks.js
+++ src/components/addBooks.js
@@ -1,13 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuid } from 'uuid';
+import { addBook } from '../redux/books/booksSlice';
 
-const AddBook = () => (
-  <div>
-    <form>
-      <input type="text" placeholder="Title" />
-      <input type="text" placeholder="Author" />
-      <input type="submit" value="Add Book" />
-    </form>
-  </div>
-);
+const AddBook = () => {
+  const dispatch = useDispatch();
+
+  const [newBook, setNewBook] = useState({ title: '', author: '' });
+
+  const handleTitle = (e) => {
+    if (e.target.value !== '') {
+      setNewBook({
+        ...newBook, title: e.target.value,
+      });
+    }
+  };
+  const handleAuthor = (e) => {
+    if (e.target.value !== '') {
+      setNewBook({
+        ...newBook, author: e.target.value,
+      });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!(newBook.title.trim() && newBook.author.trim())) {
+      return;
+    }
+
+    const book = {
+      id: uuid(),
+      title: newBook.title,
+      author: newBook.author,
+    };
+    dispatch(addBook(book));
+    setNewBook({
+      title: '',
+      author: '',
+    });
+  };
+  return (
+    <div>
+      <form>
+
+        <input
+          type="text"
+          placeholder="Title"
+          name="title"
+          value={newBook.title}
+          onChange={handleTitle}
+          required
+        />
+
+        <input
+          type="text"
+          placeholder="Author"
+          name="author"
+          value={newBook.author}
+          onChange={handleAuthor}
+          required
+        />
+
+        <button
+          type="submit"
+          onClick={handleSubmit}
+        >
+          Add Book
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default AddBook;
