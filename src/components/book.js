e14a0352f790e4367f63b1d5404a5df325c0bafa
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-04-05 20:39:01 +0300

    Update book.js

--- src/components/book.js
+++ src/components/book.js
@@ -1,50 +1,59 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/booksSlice';
 
-function Book({
-  author, title, category, bookId,
-}) {
+function Book(props) {
+  const {
+    author, title, category, bookId,
+  } = props;
   const dispatch = useDispatch();
-  const handleRemove = () => dispatch(removeBook(bookId));
+
+  const handleRemove = () => {
+    dispatch(removeBook(bookId));
+  };
+
   return (
-    <>
-      <div className="display">
-        <h2 className="book-h1">Book Store CMS</h2>
-        <div>
-          <h3>
-            Category:
-            <em>
-              **
-              { category }
-              **
-            </em>
-          </h3>
-          <h3>
-            Title:
-            <em>
-              **
-              { title }
-              **
-            </em>
-          </h3>
-          <h3>
-            Author:
-            <em>
-              **
-              { author }
-              **
-            </em>
-          </h3>
-        </div>
-        <div>
-          <button type="button" className="remove-btn">Comments</button>
-          <button type="button" className="remove-btn" onClick={handleRemove}>Remove</button>
-          <button type="button" className="remove-btn">Edit</button>
-        </div>
+    <div className="display">
+      <h2 className="book-h1">Book Store CMS</h2>
+      <div>
+        <h3>
+          Category:
+          <em>
+            **
+            {category}
+            **
+          </em>
+        </h3>
+        <h3>
+          Title:
+          <em>
+            **
+            {title}
+            **
+          </em>
+        </h3>
+        <h3>
+          Author:
+          <em>
+            **
+            {author}
+            **
+          </em>
+        </h3>
       </div>
-    </>
+      <div>
+        <button type="button" className="remove-btn">
+          Comments
+        </button>
+        <button type="button" className="remove-btn" onClick={handleRemove}>
+          Remove
+        </button>
+        <button type="button" className="remove-btn">
+          Edit
+        </button>
+      </div>
+    </div>
   );
 }
 
