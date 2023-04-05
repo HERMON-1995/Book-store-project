0c534012b786a5c6627f25dd6553139e317a4d3f
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-04-05 14:43:43 +0300

    Update displayBooks.js

--- src/components/displayBooks.js
+++ src/components/displayBooks.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { loadBooks } from '../redux/books/booksSlice';
 import Book from './book';
 
-const DisplayBooks = () => (
-  <>
-    <h1 className="book-h1">Bookstore CMS</h1>
-    <Book />
-  </>
-);
+const DisplayBooks = () => {
+  const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadBooks());
+  }, [dispatch]);
+
+  return (
+    <>
+      <div>
+        {Object.keys(books).length > 0 && Object.keys(books).map((id) => (
+          <div key={id}>
+            <Book
+              author={books[id] && books[id][0] && books[id][0].author}
+              title={books[id][0].title}
+              category={books[id][0].category}
+              bookId={id}
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default DisplayBooks;
